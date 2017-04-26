f945be36bbc9878ebcf59c6523b7b7e52ab88acb
eyegio <g.segagni@gmail.com>
2017-04-26 17:06:46 -0400

    implemented portfolio with ngFor

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+  projects = [
+    {"id": 1, "name": "MYagonism", "label": "myagonism-project", "url": "../app/images/portfolio/myagonism.png"},
+    {"id": 2, "name": "coach-Agenda", "label": "coach-agenda-project", "url": "../app/images/portfolio/coach-agenda.png"},
+    {"id": 3, "name": "Unicredit Dealer", "label": "dealer-project", "url": "../app/images/portfolio/unicredit-dealer.png"},
+    {"id": 4, "name": "Unicredit Revolution", "label": "revolution-project", "url": "../app/images/portfolio/unicredit-revolution.png"},
+    {"id": 5, "name": "Safety Bio Life", "label": "sbl-project", "url": "../app/images/portfolio/sbl.png"},
+    {"id": 6, "name": "Paparelli", "label": "paparelli-project", "url": "../app/images/portfolio/paparelli.png"},
+    {"id": 7, "name": "Fisios", "label": "fisios-project", "url": "../app/images/portfolio/fisios.png"},
+    {"id": 8, "name": "Sir", "label": "sir-project", "url": "../app/images/portfolio/sir.png"},
+    {"id": 9, "name": "Demasteel", "label": "demasteel-project", "url": "../app/images/portfolio/demasteel.png"}
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  
   }
 
 }
